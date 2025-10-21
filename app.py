4a89cecbb17402f303bd5fc320b2f8ae9824ebd8
gokulan006 <gokulan.m.min24@itbhu.ac.in>
2025-10-21 14:42:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
 from langchain.retrievers import BM25Retriever, EnsembleRetriever
@@ -176,4 +176,5 @@ if query:
      
 
     
+
 
